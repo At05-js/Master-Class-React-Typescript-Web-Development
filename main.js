f89dca5c2eb2bef23306f66a32058896cc833798
Gómez <josue-salatiel.gomez@atos.net>
2022-02-22 16:04:43 -0600

    Shallow and Deep Copy - Hands on

--- main.js
+++ main.js
@@ -1,11 +1,74 @@
-// let sample = 5;
-// let copySample = sample;
-// sample = 10;
-// console.log('sample ',sample);
-// console.log('copySample ',copySample);
+// OBJECT
 
-// let sample = {name:'Rysh'};
-// let copySample = sample;
-// sample.test = 'test';
-// console.log('sample ',sample);
-// console.log('copySample ',copySample);
+// const orig = {
+//     test: "test",
+//     userDetails: {
+//         address: {
+//             province: 'Province',
+//             country: 'country'
+//         },
+//         name: 'myObject'
+//     }
+// }
+// const copy = orig;
+// copy.test = 'newTest';
+// console.log('orig',orig);
+// console.log('copy',copy);
+
+// const orig = {
+//     test: "test",
+//     userDetails: {
+//         address: {
+//             province: 'Province',
+//             country: 'country'
+//         },
+//         name: 'myObject'
+//     }
+// }
+// const copy = Object.assign({}, orig);
+// copy.userDetails.name = 'newName';
+// console.log('orig',orig);
+// console.log('copy',copy);
+
+// const orig = {
+//     test: "test",
+//     userDetails: {
+//         address: {
+//             province: 'Province',
+//             country: 'country'
+//         },
+//         name: 'myObject'
+//     },
+//     myFn: () => {},
+//     undef: undefined,
+// }
+// const copy = JSON.parse(JSON.stringify(orig));
+// copy.userDetails.name = 'newName';
+// console.log('orig',orig);
+// console.log('copy',copy);
+
+// ARRAY
+
+// const orig = ['avocado', 'apple', { name: 'strawberry' }];
+// const copy = orig;
+// copy.push('guava');
+// console.log('orig: ', orig);
+// console.log('copy: ', copy);
+
+// const orig = ['avocado', 'apple', { name: 'strawberry' }];
+// const copy = orig.slice();
+// copy.push('guava');
+// console.log('orig: ', orig);
+// console.log('copy: ', copy);
+
+// const orig = ['avocado', 'apple', { name: 'strawberry' }];
+// const copy = orig.slice();
+// copy[2].name = 'coconut';
+// console.log('orig: ', orig);
+// console.log('copy: ', copy);
+
+const orig = ['avocado', 'apple', { name: 'strawberry' }];
+const copy = JSON.parse(JSON.stringify(orig));
+copy[2].name = 'coconut';
+console.log('orig: ', orig);
+console.log('copy: ', copy);
