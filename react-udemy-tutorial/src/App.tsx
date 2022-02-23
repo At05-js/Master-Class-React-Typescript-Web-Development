d6f1b3a8aec53701db208bd8e8ccfa63825794b8
Gómez <josue-salatiel.gomez@atos.net>
2022-02-23 16:04:35 -0600

    State - Hands On

--- react-udemy-tutorial/src/App.tsx
+++ react-udemy-tutorial/src/App.tsx
@@ -1,15 +1,14 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { Button } from './components/Button/index';
+import CounterManagement from './components/CounterManagement';
 
 function App() {
   return (
-    <React.Fragment>
+    <>
       <h1>My App</h1>
-      <Button type='primary'>Primary</Button>
-      <Button>Default</Button>
-    </React.Fragment>
+      <CounterManagement ownerName='Josue' />
+    </>
   );
 }
 
