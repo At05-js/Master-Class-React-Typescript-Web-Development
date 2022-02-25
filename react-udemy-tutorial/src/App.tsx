7c0e3510a977bd3ded866f9bca73c1369ab4d2a2
Gómez <josue-salatiel.gomez@atos.net>
2022-02-25 17:04:39 -0600

    RenderProps - Hands On

--- react-udemy-tutorial/src/App.tsx
+++ react-udemy-tutorial/src/App.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import './App.css';
-import MyPureComponent from './components/MyPureComponent';
+import { NewsFeed } from './components/NewsFeed';
+import { Profile } from './components/Profile';
+import TrackClick from './components/TrackClick';
 
 export const App:React.FC=()=>{
   return (
     <React.Fragment>
       <h1>My App</h1>
-      <MyPureComponent/>
+      <TrackClick renderProps={(click)=><Profile click={click}/>}/>
+      <TrackClick renderProps={(click)=><NewsFeed click={click}/>}/>
     </React.Fragment>
   )
 }
