42a920618fcbd1c075d5c32a9723c26eece7175d
Gómez <josue-salatiel.gomez@atos.net>
2022-03-02 14:23:45 -0600

    Unmount Component at Node API

--- react-udemy-tutorial/src/index.tsx
+++ react-udemy-tutorial/src/index.tsx
@@ -15,9 +15,11 @@ ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('reactSample')
+  document.getElementById('renderSample'), () => {console.log('it renders!')}
 );
 
+const hasUnmountRenderSampleComponent = ReactDOM.unmountComponentAtNode(document.getElementById('renderSample')!);
+console.log('hasUnmountRenderSampleComponent: ', hasUnmountRenderSampleComponent)
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
