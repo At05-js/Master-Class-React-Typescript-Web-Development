d56c77d8cc73bfc067d04624230d28f7d6ed4d00
Gómez <josue-salatiel.gomez@atos.net>
2022-03-07 16:50:24 -0600

    Create & Access Store - Hands On

--- react-udemy-tutorial/src/App.tsx
+++ react-udemy-tutorial/src/App.tsx
@@ -1,16 +1,24 @@
 import React from 'react';
 import './App.css';
 import HomePage from './components/HomePage';
-import { Route, Switch } from 'react-router';
+import { Route, Switch, BrowserRouter } from 'react-router-dom';
 import AboutPage from './components/AboutPage';
-import { BrowserRouter } from 'react-router-dom';
+import { createStore } from 'redux';
+import { fruitsReducer } from './reducer/fruitsReducer';
+import { Provider } from 'react-redux';
+
+const store = createStore(fruitsReducer, ['apple', 'avocado']);
 
 function App() {
   return (
-    <div className="App">
-      <Instruction />
-      <RoutingPractice />
-    </div>
+    <Provider store={store}>
+      <BrowserRouter>
+          <Switch>
+            <Route component={AboutPage} path="/about" />
+            <Route component={HomePage} path="/" />
+          </Switch>
+      </BrowserRouter>
+    </Provider>
   );
 }
 
