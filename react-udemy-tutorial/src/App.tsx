086319d040d2281af796044c861ab3e5598dd757
Gómez <josue-salatiel.gomez@atos.net>
2022-03-08 16:40:37 -0600

    Create our own custom middleware (Redux - Thunk like)

--- react-udemy-tutorial/src/App.tsx
+++ react-udemy-tutorial/src/App.tsx
@@ -3,12 +3,13 @@ import './App.css';
 import HomePage from './components/HomePage';
 import { Route, Switch, BrowserRouter } from 'react-router-dom';
 import AboutPage from './components/AboutPage';
-import { createStore } from 'redux';
-import { fruitsReducer } from './reducer/fruitsReducer';
+import { applyMiddleware, createStore } from 'redux';
 import { Provider } from 'react-redux';
-import { rootReducer } from './reducer/rootReducer';
+import { customMiddleware } from './store/middlewares/customMiddleware';
+import { anotherMiddleware } from './store/middlewares/anotherMiddleware';
+import { rootReducer } from './store/reducer/rootReducer';
 
-const store = createStore(rootReducer, { users: ['Josue', 'May'], fruits: ['apple', 'avocado'] });
+const store = createStore(rootReducer, { users: ['Josue', 'May'], fruits: ['apple', 'avocado'] }, applyMiddleware(customMiddleware, anotherMiddleware));
 
 function App() {
   return (
