b4c426949420818ada5b751e6e91acb18af21adc
Gómez <josue-salatiel.gomez@atos.net>
2022-03-08 16:02:53 -0600

    Combine Reducers - Hands On

--- react-udemy-tutorial/src/App.tsx
+++ react-udemy-tutorial/src/App.tsx
@@ -6,8 +6,9 @@ import AboutPage from './components/AboutPage';
 import { createStore } from 'redux';
 import { fruitsReducer } from './reducer/fruitsReducer';
 import { Provider } from 'react-redux';
+import { rootReducer } from './reducer/rootReducer';
 
-const store = createStore(fruitsReducer, ['apple', 'avocado']);
+const store = createStore(rootReducer, { users: ['Josue', 'May'], fruits: ['apple', 'avocado'] });
 
 function App() {
   return (
